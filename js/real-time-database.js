0c8cc349b80b3d30f315aac6849a90a06bfa868f
Darlan Damasio <damasio34@gmail.com>
2016-09-14 01:18:42 -0300

    Concluindo o leitura e escrita da página real-time

---
+++ js/real-time-database.js
@@ -0,0 +1,27 @@
+var txtNome =  document.getElementById('txtNome');
+var nmbIdade =  document.getElementById('nmbIdade');
+var btnAdicionar = document.getElementById('btnAdicionar');
+var listaDeUsuarios = document.getElementById('listaDeUsuarios');
+
+// Ao cliclar no botão
+btnAdicionar.addEventListener('click', function() {
+    create(txtNome.value, nmbIdade.value);
+});
+
+function create(nome, idade) {
+    var data = {
+        nome: nome,
+        idade: idade
+    };
+
+    return firebase.database().ref().child('usuarios').push(data);
+}
+
+firebase.database().ref('usuarios').on('value', function(snapshot){
+    listaDeUsuarios.innerHTML = '';
+    snapshot.forEach(function(item){
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(item.val().nome + ': ' + item.val().idade));
+        listaDeUsuarios.appendChild(li);
+    })
+})
