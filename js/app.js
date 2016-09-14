b65d57a3cde294a9e50b9562f3e99d9ca766a452
Darlan Damasio <damasio34@gmail.com>
2016-09-14 00:39:11 -0300

    Commit inicial

---
+++ js/app.js
@@ -0,0 +1,11 @@
+<script>
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyByGtDHZGY2GGbU-Jt8wvZydBeH0cZkQWE",
+        authDomain: "curso-fb-damasio34.firebaseapp.com",
+        databaseURL: "https://curso-fb-damasio34.firebaseio.com",
+        storageBucket: "curso-fb-damasio34.appspot.com",
+        messagingSenderId: "213804999061"
+    };
+    firebase.initializeApp(config);
+</script>
